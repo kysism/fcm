ce13921267263798a98308e8975106001411986e
kysism <kysism@gmail.com>
2026-05-19 23:47:24 +0300

    urlencoded

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const path = require("path");
 const app = express();
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 /* API FIRST */
 app.use("/api/users", require("./routes/userRoutes"));
