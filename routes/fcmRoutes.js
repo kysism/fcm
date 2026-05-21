daf7f3fd88bd2da9838abe81b98e7b5a401ec328
kysism <kysism@gmail.com>
2026-05-21 23:57:49 +0300

    back to last sources

--- routes/fcmRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-const fcm = require("../controllers/fcmController");
-
-router.post("/send", fcm.sendPush);
-
-module.exports = router;
--- routes/pushMessageRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-
-const router = express.Router();
-
-const controller = require("../controllers/pushMessageController");
-
-router.post("/", controller.createMessage);
-
-module.exports = router;
