9381efc58e89dc5507fe89ae891d52754703a3c9
kysism <kysism@gmail.com>
2026-05-30 09:44:16 +0300

    cors

--- server.js
+++ server.js
@@ -2,9 +2,11 @@ const express = require("express");
 const path = require("path");
 
 const app = express();
+const cors = require("cors");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 
 /* API FIRST */
 app.use("/api/auth", require("./routes/authRoutes"));
